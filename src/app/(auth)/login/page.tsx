a7402fa8295c6ef45962671ac8dc3bf9d9b4f86b
huzaifa-koder <huzaifa.tanveer@launchbox.pk>
2026-07-29 18:16:24 +0500

    feat: getting refereal code from link

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,16 @@
 "use client"
-import React from 'react'
+import React, { Suspense } from 'react'
 import LoginPage from '../_components/login';
 
 type Props = {}
 
 const Page = (props: Props) => {
   return (
-    <div className="w-full lg:w-1/2"><LoginPage /></div>
+    <div className="w-full lg:w-1/2">
+      <Suspense fallback={null}>
+        <LoginPage />
+      </Suspense>
+    </div>
   )
 }
 
