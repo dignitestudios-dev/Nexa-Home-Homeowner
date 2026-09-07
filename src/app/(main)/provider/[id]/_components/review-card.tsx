7b159c6784fe4b59a4bcddf78e53af0d42878dde
unknown <huzaifa.tanveer@dexnive.com>
2026-09-07 02:28:59 -0700

    UAT changes

--- src/app/(main)/provider/[id]/_components/review-card.tsx
+++ src/app/(main)/provider/[id]/_components/review-card.tsx
@@ -12,7 +12,7 @@ export default function ReviewCard({ name, date, review, image }: ReviewCardProp
     <div className="relative w-full pt-10">
       {/* Avatar */}
       <div className="absolute left-5 top-0 z-10">
-        <div className="h-[122px] w-[122px] overflow-hidden rounded-full border-4 border-white bg-white shadow-sm">
+        {/* <div className="h-[122px] w-[122px] overflow-hidden rounded-full border-4 border-white bg-white shadow-sm">
           {image ? (
             <img src={image} alt={name} className="h-full w-full object-cover" />
           ) : (
@@ -20,17 +20,17 @@ export default function ReviewCard({ name, date, review, image }: ReviewCardProp
               <User className="h-12 w-12 text-white" />
             </div>
           )}
-        </div>
+        </div> */}
       </div>
 
       {/* Card */}
       <div className="rounded-[34px] bg-[#eaf0f1] h-full px-5 pb-5 pt-2 text-black">
-        <div className="ml-[130px]">
-          <h3 className="text-[20px] font-bold leading-[25px]">{name}</h3>
+        <div className="mt-4">
+          <h3 className="text-[20px] font-bold leading-[25px]">{name.split(" ")[0] + " " + name.split(" ")[1].slice(0, 1).toUpperCase()}</h3>
           <p className="mt-1 text-[16px]   font-semibold  text-black/70">{date}</p>
         </div>
         <p className="mt-8 text-[16px] break-all font-semibold leading-[26px] text-black/50">{review}</p>
       </div>
     </div>
-  );
+  );  
 }
